03226dd2ed668fb1c5c963dfb919c29c1e5702b4
jihee-yun <dandelion1141@gmail.com>
2023-06-23 19:31:57 +0900

    6.23일 웹시큐리티 및 카페 관련 수정

--- src/main/java/com/kh/finalProject/controller/CafeController.java
+++ src/main/java/com/kh/finalProject/controller/CafeController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +14,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/cafe")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CafeController {
     private final CafeService cafeService;
 
--- src/main/java/com/kh/finalProject/entity/Cafe.java
+++ src/main/java/com/kh/finalProject/entity/Cafe.java
@@ -18,14 +18,33 @@ public class Cafe {
     @Column(name = "cafe_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String cafeName;
+
+    @Column(nullable = false, length = 1000)
     private String thumbnail;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false, length = 1000)
     private String detailIntro;
+
+    @Column(nullable = false)
     private String intro;
+
+    @Column(nullable = false, length = 30)
     private String region;
+
+    @Column(nullable = false, length = 50)
     private String tel;
+
+    @Column(nullable = false)
     private Long likeCount;
+
+    @Column(nullable = false)
+    private String operatingTime; // 운영 시간
 
     @OneToMany(mappedBy = "cafe")
     private List<CafeImg> cafeImgList = new ArrayList<>();

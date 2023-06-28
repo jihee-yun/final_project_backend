73e3903bceff665cc8c362c0cccd1fa9642a35fe
KwakJaehyung <kjh213213@gmail.com>
2023-06-28 23:53:02 +0900

    06 /28 수 크로스오리진 임시 추가

--- src/main/java/com/kh/finalProject/controller/ReviewController.java
+++ src/main/java/com/kh/finalProject/controller/ReviewController.java
@@ -6,16 +6,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/review")
 public class ReviewController {
     private final ReviewService reviewService;

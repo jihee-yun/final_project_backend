c44a9312aae4d1cd2190675b74853e0353da237f
koth1999 <83741577+koth1999@users.noreply.github.com>
2023-06-30 18:17:18 +0900

    WebSecurityConfig 수정
    WebSecurityConfig 수정

--- src/main/java/com/kh/finalProject/security/WebSecurityConfig.java
+++ src/main/java/com/kh/finalProject/security/WebSecurityConfig.java
@@ -16,38 +16,39 @@ import org.springframework.stereotype.Component;
 @Configuration
 @EnableWebSecurity
 @Component
+
 public class WebSecurityConfig {
-    private final TokenProvider tokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+        private final TokenProvider tokenProvider;
+        private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+        private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable() // HTTP 기본 인증을 하지 않도록 설정
-                .csrf().disable() // 위변조 관련 보호 정책을 비활성화
-                //세션을 사용하지 않고, 상태를 유지하지 않는 세션 관리 정책을 설정
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic().disable() // HTTP 기본 인증을 하지 않도록 설정
+                    .csrf().disable() // 위변조 관련 보호 정책을 비활성화
+                    //세션을 사용하지 않고, 상태를 유지하지 않는 세션 관리 정책을 설정
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
+                    .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
 
-                .and()
-                .authorizeRequests()
-                // 경로에 대해 인증 없이 접근을 허용
-                .antMatchers("/auth/**", "/mypage/**", "/review/**", "/cafe/**", "/event/**", "/kakao/**", "/couponstore/**").permitAll() // 어떤 패스로 들어올 때 접근을 허용해줄건지
-                .anyRequest().authenticated()
+                    .and()
+                    .authorizeRequests()
+                    // 경로에 대해 인증 없이 접근을 허용
+                    .antMatchers("/auth/**", "/mypage/**", "/review/**", "/cafe/**", "/event/**", "/kakao/**", "/couponstore/**", "/guild/**").permitAll() // 어떤 패스로 들어올 때 접근을 허용해줄건지
+                    .anyRequest().authenticated()
 
-                .and()
-                .apply(new JwtSecurityConfig(tokenProvider));
+                    .and()
+                    .apply(new JwtSecurityConfig(tokenProvider));
 
-        return http.build();
-    }
+            return http.build();
+        }
 }

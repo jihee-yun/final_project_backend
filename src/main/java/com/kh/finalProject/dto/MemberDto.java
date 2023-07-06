a7f545ba324c9598dce86fc719cdeeb58d07d108
JAEHYUNGKWAK <126457219+JAEHYUNGKWAK@users.noreply.github.com>
2023-07-06 19:01:30 +0900

    07/06 목 memberDto 수정

--- src/main/java/com/kh/finalProject/dto/MemberDto.java
+++ src/main/java/com/kh/finalProject/dto/MemberDto.java
@@ -3,11 +3,16 @@ package com.kh.finalProject.dto;
 import com.kh.finalProject.constant.Authority;
 import com.kh.finalProject.constant.Existence;
 import com.kh.finalProject.constant.Gender;
+import com.kh.finalProject.entity.Guild;
+import com.kh.finalProject.entity.GuildMember;
+import com.kh.finalProject.entity.Point;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,11 +25,22 @@ public class MemberDto {
     private String email;
     private Date birthday; // 생년월일
     private Gender gender; // 성별
-    private LocalDateTime signUpTime; // 가입 시간
+    private LocalDate signUpTime; // 가입 시간
     private Long followingId; // 내가 팔로우한 아이디
     private Long followedId; // 나를 팔로우한 아이디
     private String profileImgUrl; // 프로필 이미지 url
     private String intro; // 한 줄 소개
     private Existence existence; // 탈퇴 여부
     private Authority authority; // 회원 종류 구분
+
+    // 길드 테이블 관련
+    private List<Guild> guildList = new ArrayList<>();
+    private List<GuildMember> guildMemberList = new ArrayList<>();
+
+    // 포인트 테이블 관련
+    private Point point;
+
+
+
+
 }

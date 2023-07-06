e1859fde3aed5d598415816a64f930bbba9efa22
yeab0824@naver.com <http://github.com/yeab0824>
2023-07-06 19:28:53 +0900

    230706

--- src/main/java/com/kh/finalProject/dto/PointDto.java
+++ src/main/java/com/kh/finalProject/dto/PointDto.java
@@ -1,4 +1,11 @@
 package com.kh.finalProject.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PointDto {
+    private int totalPoint;
+    private String name;
 }
--- src/main/java/com/kh/finalProject/service/PointService.java
+++ src/main/java/com/kh/finalProject/service/PointService.java
@@ -1,5 +1,6 @@
 package com.kh.finalProject.service;
 
+import com.kh.finalProject.dto.PointDto;
 import com.kh.finalProject.entity.Point;
 import com.kh.finalProject.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -15,10 +18,23 @@ import javax.transaction.Transactional;
 public class PointService {
     public final PointRepository pointRepository;
 
+    // 포인트 적립
     public boolean addPoint(int totalPoint) {
         Point point = new Point();
         point.setTotalPoint(totalPoint);
         Point savePoint = pointRepository.save(point);
         return true;
+    }
+
+    // 포인트 조회
+    public List<PointDto> getPointList() {
+        List<Point> points = pointRepository.findAll();
+        List<PointDto> pointDtoList = new ArrayList<>();
+        for (Point point : points) {
+            PointDto pointDto = new PointDto();
+            pointDto.setTotalPoint(point.getTotalPoint());
+            pointDto.setName(point.getUser().getName());
+        }
+        return pointDtoList;
     }
 }

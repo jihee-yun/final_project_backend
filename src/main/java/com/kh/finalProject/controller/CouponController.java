54274dfa2d0eb8eac438162e5307f67f8734d768
yea-bin <yeab0824@naver.com>
2023-07-10 20:11:33 +0900

    쿠폰 컨트롤러 수정

--- src/main/java/com/kh/finalProject/controller/CouponController.java
+++ src/main/java/com/kh/finalProject/controller/CouponController.java
@@ -1,7 +1,11 @@
 package com.kh.finalProject.controller;
 
 import com.kh.finalProject.dto.CouponDto;
+import com.kh.finalProject.dto.MemberDto;
+import com.kh.finalProject.dto.UserDto;
 import com.kh.finalProject.service.CouponService;
+import com.kh.finalProject.service.MemberService;
+import com.kh.finalProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,11 +21,19 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class CouponController {
     public final CouponService couponService;
+    public final UserService userService;
 
     @GetMapping("/couponget")
     public ResponseEntity<List<CouponDto>> couponList(@RequestParam String couponget) {
         System.out.println("couponget" + couponget);
         List<CouponDto> list = couponService.selectCouponList();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/myInfo")
+    public ResponseEntity<List<UserDto>> infoList(@RequestParam String myinfo) {
+        System.out.println("myinfo" + myinfo);
+        List<UserDto> list = userService.getMemberList();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 

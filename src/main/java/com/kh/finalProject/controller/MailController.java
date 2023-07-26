5457418cb2b62d83ecef4c317e10bebc89aa23d9
jihee-yun <dandelion1141@gmail.com>
2023-07-26 13:54:15 +0900

    7.26일 이메일 인증 구현

---
+++ src/main/java/com/kh/finalProject/controller/MailController.java
@@ -0,0 +1,35 @@
+package com.kh.finalProject.controller;
+
+import com.kh.finalProject.service.MailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/mail")
+@CrossOrigin(origins = "http://localhost:3000")
+public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/confirm")
+    public ResponseEntity<Boolean> mailConfirm(@RequestBody Map<String, String> mailData) {
+        String mail = mailData.get("mail");
+        boolean result = mailService.confirm(mail);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<Boolean> verifyCode(@RequestBody Map<String, String> verifyData) {
+        String mail = verifyData.get("mail");
+        String code = verifyData.get("code");
+        boolean result = mailService.verifyCode(mail, code);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/kh/finalProject/service/MailService.java
@@ -0,0 +1,56 @@
+package com.kh.finalProject.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private static final String senderEmail= "dandelion1141@gmail.com";
+    private static int number;
+    private static Map<String, String> verificationCodes = new HashMap<>();
+
+    public static void createNumber(String mail){
+        number = (int)(Math.random() * (90000)) + 100000;
+        verificationCodes.put(mail, String.valueOf(number));
+    }
+
+    public MimeMessage CreateMail(String mail){
+        createNumber(mail);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        try {
+            message.setFrom(senderEmail);
+            message.setRecipients(MimeMessage.RecipientType.TO, mail);
+            message.setSubject("이메일 인증");
+            String body = "";
+            body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
+            body += "<h1>" + number + "</h1>";
+            body += "<h3>" + "감사합니다." + "</h3>";
+            message.setText(body,"UTF-8", "html");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+    public boolean confirm(String mail) {
+        MimeMessage message = CreateMail(mail);
+        javaMailSender.send(message);
+        return true;
+    }
+
+    public boolean verifyCode(String mail, String code) {
+        String savedCode = verificationCodes.get(mail);
+        return code.equals(savedCode);
+    }
+
+}
